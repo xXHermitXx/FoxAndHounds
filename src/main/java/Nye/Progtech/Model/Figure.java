07333cf23d510d19e680a8e485d2884d67e279ea
Lőrinczi Máté <78899823+xXHermitXx@users.noreply.github.com>
2022-10-17 15:53:58 +0200

    Add files via upload

---
+++ src/main/java/Nye/Progtech/Model/Figure.java
@@ -0,0 +1,14 @@
+
+package business_logic;
+
+/**
+ * It is a common interface for the figures that reside at the table (fox, hounds).
+ * @author valyis
+ */
+public interface Figure {
+    int getX();
+    int getY();
+    void setX(int x);
+    void setY(int a);
+    
+}
